4bbc0d4b6540a4836fb438528a29648565ac6666
NikhlEi <nikhilc@eigroup.in>
2026-08-21 10:45:54 +0530

    soem new changes

--- src/components/SiteForms.tsx
+++ src/components/SiteForms.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef } from "react";
 
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000/api";
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4010/api";
 const EMAIL_RE = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const MOBILE_RE = /^[0-9]{6,20}$/;
 
